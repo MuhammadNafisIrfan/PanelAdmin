af7c5b28ffd94e469b1394362e45cf293cd9f093
M.NafisIrfan <nafizbbx@gmail.com>
2020-01-14 11:48:24 +0700

    b

--- ustad.php
+++ ustad.php
@@ -3,6 +3,29 @@ include('includes/header.php');
 include('includes/navbar.php'); 
 ?> 
 
+<div class="modal fade" id="addadminprofile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Apakah anda yakin ingin menon-aktifkan guru yang telah dipilih ? </h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form action="code.php" method="POST">
+
+    
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+            <button type="submit" name="registerbtn" class="btn btn-primary">Yes</button>
+        </div>
+      </form>
+
+    </div>
+  </div>
+</div>
+
+
         <!-- Begin Page Content -->
         <div class="container-fluid">
 
@@ -55,15 +78,86 @@ include('includes/navbar.php');
                   <td> <?php echo $row['biografi_guru']; ?> </td>
                   <td> <?php echo $row['rating']; ?> </td>
                   <td> <?php echo $row['total_santri']; ?> </td>
-                  <td> <?php echo $row['total_pertemuan']; ?> </td>     
+                  <td> <?php echo $row['total_pertemuan']; ?> </td>
                   <td>
-                <form action="register_edit.php" method="post">
-                <toggle type="button" class="btn btn-primary" data-toggle="modal" data-target="#offmodal"> ON </toggle>
-                </form>
-                <form action="register_edit.php" method="post">
-                <toggle type="button" class="btn btn-primary" data-toggle="modal" data-target="#addadminprofile">OFF</toggle>
-                </form>
-            </td>                              
+                  <style>
+                          .switch {
+                            
+                            position: relative;
+                            display: inline-block;
+                            width: 60px;
+                            height: 34px;
+                          }
+
+                          .switch input { 
+                            content: "On";
+                            opacity: 0;
+                            width: 0;
+                            height: 0;
+                          }
+
+                          .slider {
+                            position: absolute;
+                            cursor: pointer;
+                            top: 0;
+                            left: 0;
+                            right: 0;
+                            bottom: 0;
+                            background-color: #ccc;
+                            -webkit-transition: .4s;
+                            transition: .4s;
+                          }
+
+                          .slider:before {
+                            position: absolute;
+                            content: "";
+                            height: 26px;
+                            width: 26px;
+                            left: 4px;
+                            bottom: 4px;
+                            background-color: white;
+                            -webkit-transition: .4s;
+                            transition: .4s;
+                          }
+
+                          input:checked + .slider {
+                            background-color: #2196F3;
+                          }
+
+                          input:focus + .slider {
+                            box-shadow: 0 0 1px #2196F3;
+                          }
+
+                          input:checked + .slider:before {
+                            -webkit-transform: translateX(26px);
+                            -ms-transform: translateX(26px);
+                            transform: translateX(26px);
+                          }
+
+                          /* Rounded sliders */
+                          .slider.round {
+                            border-radius: 34px;
+                          }
+
+                          .slider.round:before {
+                            border-radius: 50%;
+                          }
+                          </style>
+                          </head>
+                          <body>
+
+
+                          <label class="switch" data-toggle="modal" data-target="#addadminprofile">
+                            <input type="checkbox" checked>
+                            <span class="slider round"></span>
+                          </label>
+                          </td> 
+
+
+                  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+                            
                   <!-- <td>
                     <input type="checkbox">
                     <label for="" class="onbtn">On</label>
